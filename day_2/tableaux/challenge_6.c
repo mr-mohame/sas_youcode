0458432f60fead8b451bf4de8708429e614a516f
mr-mohame <mohamed-messi_fcb@hotmail.fr>
2025-09-03 15:20:27 +0100

    challenge_6

---
+++ day_2/tableaux/challenge_6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+//Challenge 6 : Multiplication des Élémenen
+int main(){
+int taille, i; 
+printf("---------donne la taille de tableaux-------\n");
+printf("donner la taille\n");
+scanf("%d", &taille);
+int T[taille];
+printf("-----------remplir le tableaux--------------\n");
+for( i=0; i< taille; i++){
+    printf("l'element %d:  ", i+1);
+    scanf("%d", &T[i]);
+}
+int facteur;
+printf("---------donne le facteur-------\n");
+printf("donner le facteur de multiplication: ");
+scanf("%d", &facteur);
+printf("---------calcule la multuplication-------\n");
+for(i=0; i<taille; i++){
+    int mult = T[i] * facteur;
+    printf("%d * %d = %d\n", T[i], facteur, T[i] * facteur);
+}
+    return 0;
+}
